fe9cc19a276303402e9f30085c89f8bf59b6d8ce
BelisnalvaCosta <bella-costa@hotmail.com.br>
2024-03-25 23:31:02 -0300

    Committing packages and files Champions and ChampionsRepository

---
+++ santander-dev-week-2024/src/main/java/me/dio/sdw2024/domain/model/ChampionsTradicional.java
@@ -0,0 +1,59 @@
+// Não vamos ussar este modo tradicional de POO, mas com o record
+/*package me.dio.sdw2024.domain.model;
+
+public class ChampionsTradicional {
+    private Long id;
+    private String name;
+    private String role;
+    private String lore;
+    private String imageUrl;
+
+    public ChampionsTradicional(Long id, String name, String role, String lore, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.lore = lore;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
+*/
---
+++ santander-dev-week-2024/src/main/java/me/dio/sdw2024/domain/ports/ChampionsRepository.java
@@ -0,0 +1,12 @@
+package me.dio.sdw2024.domain.ports;
+
+import me.dio.sdw2024.domain.model.Champions;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChampionsRepository {
+    List<Champions> findAll();
+
+    Optional<Champions> findById(Long id);
+}
